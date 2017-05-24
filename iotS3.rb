c470cc70803e1d4781fdaaa0bcb6bc6935b417f3
pitomtan <tanaka1201t@gmail.com>
2017-05-24 10:06:25 +0900

    change output-format to JSON

--- iotS3.rb
+++ iotS3.rb
@@ -20,6 +20,12 @@ class RasPiIotS3
     #i2c
     @device = I2C.create(path)
     @address = address
+
+    @time = 0
+    @dt = 0 #finishtime - starttime
+    @starttime = 0
+    @finishtime = 0
+    @tini = 0
   end
 
   #fetch Humidity & Temperature with i2c device
@@ -28,25 +34,33 @@ class RasPiIotS3
     hum_h, hum_l, temp_h, temp_l = s.bytes.to_a
 
     status = (hum_h >> 6) & 0x03
-    time = Time.now.getlocal
+    @time = Time.now.getlocal
     hum_h = hum_h & 0x3f
     hum = (hum_h << 8) | hum_l
     temp = ((temp_h << 8) | temp_l) / 4
 
-    temperature = temp * 1.007e-2 - 40.0
-
+    @temp = temp * 1.007e-2 - 40.0
+    
+    outputjson = JSON.generate({"time" => @time, "temp" => @temp})
     #return "time=#{time}","status=#{status}", "Humidity=#{hum* 6.10e-3}", "Temperature=#{temp * 1.007e-2 - 40.0}","\n"
-    return "#{time},#{temperature}"
+    #return "{\"time\":#{@time},\"temp\":#{@temp}}"
+    return outputjson
   end
-
+=begin
   def makingcsv
-    time = Time.now.getlocal
-
-    File.open("iotTempLog_#{time}.csv", "w") do |first_line|
-        first_line.write "time, temp"
+    @starttime = Time.now.getlocal
+    puts @starttime
+    File.open("iotTempLog_#{@starttime}.csv", "w") do |first_line|
+        first_line.write "time, temp\n"
     end #File.open(resultfile, "w") do |result| end
   end
 
+  def addDataCsv
+    File.open("iotTempLog_#{@starttime}.csv", "a") do |data_line|
+      data_line.write "#{@time}, #{@temp}\n" #Write Tini, Tend, dt
+    end
+  end
+=end
   #Output data to AWSIoT
   def outputData
     inputData = fetch_humidity_temperature
@@ -62,8 +76,7 @@ sensingWithRaspi = RasPiIotS3.new('/dev/i2c-1')
 
 loop do
   puts sensingWithRaspi.fetch_humidity_temperature
-  #sensingWithRaspi.outputData
-  sleep(3)
+  sensingWithRaspi.outputData
+  #sleep(3)
 end
 
-sensingWithRaspi.makingcsv
