2a33863546699844e34f62b7c0caf6a779b3f41d
Tomohiro TANAKA <tanaka1201t@gmail.com>
2017-05-28 20:30:37 +0900

    Subscribe test from local PC

---
+++ iotShadowSubTest.rb
@@ -0,0 +1,71 @@
+require 'mqtt'
+require 'i2c'
+require 'fileutils'
+require 'json'
+require 'pp'
+require 'date'
+require 'yaml'
+
+class RasPiIotShadow
+  attr_accessor :airconmode
+  def initialize(path, address = 0x27, airconmode= 0)
+    #AWSIoT Read yaml
+    iotconfig = YAML.load_file("iotShadow.yml")
+    @host = iotconfig["iotShadowConfig"]["host"]
+    @topic = iotconfig["iotShadowConfig"]["topic"]
+    @port = iotconfig["iotShadowConfig"]["port"]
+    @certificate_path = iotconfig["iotShadowConfig"]["certificatePath"]
+    @private_key_path = iotconfig["iotShadowConfig"]["privateKeyPath"]
+    @root_ca_path = iotconfig["iotShadowConfig"]["rootCaPath"]
+    @thing = iotconfig["iotShadowConfig"]["thing"]
+    #i2c
+    @device = I2C.create(path)
+    @address = address
+
+    @time = 0
+    @temp = 0
+    @humidity = 0
+    @dt = 0 #finishtime - starttime
+    @starttime = 0
+    @finishtime = 0
+    @tini = 0
+
+    @airconmode = airconmode #エアコンが起動時と終了時のみ1．それ以外は0
+  end
+
+  #fetch Humidity & Temperature with i2c device
+  def fetch_humidity_temperature
+    s = @device.read(@address, 0x04)
+    hum_h, hum_l, temp_h, temp_l = s.bytes.to_a
+
+    status = (hum_h >> 6) & 0x03
+    @time = Time.now.to_i
+    hum_h = hum_h & 0x3f
+    hum = (hum_h << 8) | hum_l
+    temp = ((temp_h << 8) | temp_l) / 4
+
+    @temp = temp * 1.007e-2 - 40.0
+    @humidity  = hum * 6.10e-3
+    outputjson = JSON.generate({"datetime" => @time, "temp" => @temp, "airconmode" => @airconmode})
+    return outputjson
+  end
+
+#Output data to AWSIoT
+  def outputData
+    inputData = fetch_humidity_temperature
+    MQTT::Client.connect(host:@host, port:@port, ssl: true, cert_file:@certificate_path, key_file:@private_key_path, ca_file: @root_ca_path) do |client|
+    client.publish(@topic, inputData)
+    end
+  end
+end
+  #Setting of output "iotTempLog_${timestamp()}.csv"
+
+#Following are processed codes
+sensingWithRaspi = RasPiIotShadow.new('/dev/i2c-1')
+
+sensingWithRaspi.airconmode = 0 #shadowでonになった時
+loop do
+  puts sensingWithRaspi.fetch_humidity_temperature
+  sensingWithRaspi.outputData
+  #sleep(3)
+end
