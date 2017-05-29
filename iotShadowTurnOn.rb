9cc8beea47434ee7176702bee8d6bc35cd14827f
Tomohiro TANAKA <tanaka1201t@gmail.com>
2017-05-29 10:29:36 +0900

    Add daemon process

---
+++ iotShadowTurnOn.rb
@@ -0,0 +1,59 @@
+require 'mqtt'
+require 'i2c'
+require 'fileutils'
+require 'json'
+require 'pp'
+require 'date'
+require 'yaml'
+
+class RasPiIotShadow
+  attr_accessor :airconmode
+  def initialize(path, address = 0x27, airconmode= 0)
+    #i2c
+    @device = I2C.create(path)
+    @address = address
+    @time = 0
+    @temp = 0
+    @humidity = 0
+
+    #airconSetting
+    @setTemp = 20
+    @airconmode = airconmode #TunrnedOnAircon -> 1, TurnedOffAircon -> 0
+
+    iotconfig = YAML.load_file("iot.yml")
+    #toKinesis and tempChecker
+    @host = iotconfig["iotShadowConfig"]["host"]
+    @topic = iotconfig["iotShadowConfig"]["topic"]
+    @port = iotconfig["iotShadowConfig"]["port"]
+    @certificate_path = iotconfig["iotShadowConfig"]["certificatePath"]
+    @private_key_path = iotconfig["iotShadowConfig"]["privateKeyPath"]
+    @root_ca_path = iotconfig["iotShadowConfig"]["rootCaPath"]
+    @thing = iotconfig["iotShadowConfig"]["thing"]
+
+    #turnOnAircon and turnOffAircon
+    @topicTurnedOn = iotconfig["airconConfig"]["topicOn"]
+    @topicTurnedOff = iotconfig["airconConfig"]["topicOff"]
+  end
+
+  def turnOnAircon
+    MQTT::Client.connect(host:@host, port:@port, ssl: true, cert_file:@certificate_path, key_file:@private_key_path, ca_file: @root_ca_path) do |client|
+      puts "waiting turnonCommand.."
+      client.subscribe(@topicTurnedOn)
+      client.get #ここでturnOn.sh
+      client.publish(@topicTurnedOn, "TurnOn") #For Debug
+    end #MQTT end
+  end #def turnOnAircon end
+
+end #class RasPiIotShadow end
+
+
+#Following are processed codes
+sensingWithRaspi = RasPiIotShadow.new('/dev/i2c-1')
+
+Process.daemon
+#turnOnAircon process
+loop do
+  puts sensingWithRaspi.airconmode
+  sensingWithRaspi.turnOnAircon
+  puts "Turn On"#exec .sh command (airconOn)
+end
