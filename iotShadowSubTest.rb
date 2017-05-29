74cda8a37fb8045e4da3a4166d5a22931c30df2d
Tomohiro TANAKA <tanaka1201t@gmail.com>
2017-05-29 09:42:59 +0900

    Add pub/sub test function

--- iotShadowSubTest.rb
+++ iotShadowSubTest.rb
@@ -18,13 +18,16 @@ class RasPiIotShadow
     @root_ca_path = iotconfig["iotShadowTestConfig"]["rootCaPath"]
     @thing = iotconfig["iotShadowTestConfig"]["thing"]
 
+    @topicOn = iotconfig["iotAirconTestCongig"]["topicOn"]
+    @topicOff = iotconfig["iotAirconTestCongig"]["topicOff"]
+
     @messageHello = "Hello"
     @messageWorld = "World"
   end
 
   #fetch Humidity & Temperature with i2c device
   def pubMessageTest
-    outputjson = "Turn on Aircon"#JSON.generate({"Ruby" => @messageHello, "Swift" => @messageWorld})
+    outputjson = "Turn off Aircon"#JSON.generate({"Ruby" => @messageHello, "Swift" => @messageWorld})
     return outputjson
   end
 
@@ -32,17 +35,18 @@ class RasPiIotShadow
   def outputData
     inputData = pubMessageTest
     MQTT::Client.connect(host:@host, port:@port, ssl: true, cert_file:@certificate_path, key_file:@private_key_path, ca_file: @root_ca_path) do |client|
-    client.publish(@topic, inputData)
+    #client.publish(@topic, inputData)
+    client.publish(@topicOff, inputData)
+    client.subscribe(@topicOff)
+    puts client.get
     end
   end
 end
-  #Setting of output "iotTempLog_${timestamp()}.csv"
+
 
 #Following are processed codes
 sensingWithRaspi = RasPiIotShadow.new
 
-#loop do
-  puts sensingWithRaspi.pubMessageTest
-  sensingWithRaspi.outputData
 
-#end
+puts sensingWithRaspi.pubMessageTest
+sensingWithRaspi.outputData
